b0b6ffed328789c7aeaa4b236affa031bdf62d30
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-22 13:46:46 +0900

    (feat) 가위바위보 게임
    현재까지 완성한 기능
    - 플레이어 1과 플레이어 2가 가위, 바위, 보 중 하나를 선택하면 결과를 알려준다.
    - 결과 버튼을 누르면 결과가 표시된다.
    - 초기화 버튼을 누르면 설정된 값이 초기화된다.

---
+++ singleDart/rcp/bin/rcp.dart
@@ -0,0 +1,5 @@
+import 'package:rcp/rcp.dart' as rcp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rcp.calculate()}!');
+}
---
+++ singleDart/rockS/rockgame/android/app/src/main/kotlin/com/example/rockgame/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rockgame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
