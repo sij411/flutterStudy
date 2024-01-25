ce290f75fab8f009e5e1e76535e3fd66ae50fd72
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-25 16:11:06 +0900

    (feat) 오늘의 운세 데이터 텍스트 클렌징

---
+++ daily_fortune_teller/python_scripts/cleanse_data.py
@@ -0,0 +1,17 @@
+1# python location 
+2# -*- coding: utf-8 -*-
+
+
+cleansed = []
+with open('fortune_data/luck_data.txt', encoding="utf-8") as dt:
+    read_dt = dt.readlines()
+    for sentence in read_dt:
+        cleansed.append(sentence.strip().replace('"', ''))
+    
+
+print(len(cleansed))
+
+with open('fortune_data/cleansed_luck_data.txt', 'w', encoding="utf-8") as cl:
+    for sentence in cleansed:
+        cl.write(sentence + "\n")
+    
