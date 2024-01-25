25a0c75c2703194ea99d5f913f3ada237974ef4c
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-25 16:51:18 +0900

    images 데이터 추가

--- daily_fortune_teller/python_scripts/cleanse_data.py
+++ daily_fortune_teller/python_scripts/cleanse_data.py
@@ -14,4 +14,7 @@ print(len(cleansed))
 with open('fortune_data/cleansed_luck_data.txt', 'w', encoding="utf-8") as cl:
     for sentence in cleansed:
         cl.write(sentence + "\n")
-    
+
+with open('fortune_data/prompts_for_image.txt', 'w', encoding="utf-8") as cl:
+    for sentence in cleansed:
+        cl.write(sentence + "이 문장과 어울리는 동화책 스타일의 그림 그려줘" +"\n")
