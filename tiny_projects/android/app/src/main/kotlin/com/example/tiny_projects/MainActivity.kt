d682b6f202a388466b4a2068354346f6314809e7
Jiwon Kwon <kjiwon411@gmail.com>
2024-02-16 18:32:22 +0900

    미니 프로젝트 모아놓을 플러터 프로젝트 생성

---
+++ tiny_projects/android/app/src/main/kotlin/com/example/tiny_projects/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tiny_projects
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ tiny_projects/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+
+
+ class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color(0xFF1F1F1F),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold();
+      }
+ }
