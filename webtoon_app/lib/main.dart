fcf6e4f87fa942758b2be92b160b1d241d70fff6
Jiwon Kwon <kjiwon411@gmail.com>
2024-02-18 19:06:49 +0900

    webtoon_app(feat) make appbar with app title

--- webtoon_app/lib/main.dart
+++ webtoon_app/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon_app/screens/home_screen.dart';
 
-void main() => const MyApp();
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key}); // identifier role like an ID
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
---
+++ webtoon_app/lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 24,
+          ),
+        ),
+        elevation: 10,
+        shadowColor: Colors.grey[300],
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+      ),
+    );
+  }
+}
