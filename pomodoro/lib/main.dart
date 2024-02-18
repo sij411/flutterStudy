d8e71718a7d3ec4a1fea3503a15f1c7432ee5cb8
Jiwon Kwon <kjiwon411@gmail.com>
2024-02-18 13:49:34 +0900

    pomodoro(init) init the project

---
+++ pomodoro/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pomodoro App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+        textTheme: const TextTheme(
+            displayLarge: TextStyle(
+          color: Color(0xFF232B55),
+        )),
+        cardColor: const Color(0xFFF4EDDB),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Pomodoro'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).canvasColor,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '1',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- tiny_projects/lib/main.dart
+++ tiny_projects/lib/main.dart
@@ -1,19 +1,21 @@
+// 날짜를 선택하는 셀렉트박스 만들기
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-
-
- class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xFF1F1F1F),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const Scaffold();
-      }
- }
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: const Color(0xFF1F1F1F),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const Scaffold(
+          
+        ));
+  }
+}
